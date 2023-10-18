929b30c0a9dcc66503af97ba4694421c890dab5e
William Chen <billchen314@gmail.com>
2023-10-17 18:25:13 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path='/'/>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
